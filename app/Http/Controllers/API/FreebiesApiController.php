58d4f1da99a3dbf13b54229987afa8f3cf2006a8
haykins <akinsholasamuel@gmail.com>
2019-02-05 11:16:43 +0100

    Update Freebies API for Single Board Location

--- app/Http/Controllers/API/FreebiesApiController.php
+++ app/Http/Controllers/API/FreebiesApiController.php
@@ -37,7 +37,7 @@ class FreebiesApiController extends BaseController
         $today          = Carbon::today();
 
         // $freebies       = Pins::where('location_id', 0)->get(); 
-        $where_array    = array('at_pins.show_at' => $today->toDateString(), 'at_pins.is_active' => 1, 'location_id' => 0);
+        $where_array    = array('at_pins.show_at' => $today->toDateString(), 'at_pins.is_active' => 1, 'at_pins.location_id' => 0);
         $freebies       = DB::table('at_pins')
                             ->join('at_networks', 'at_networks.id', '=', 'at_pins.network_id')
                             ->join('sponsors', 'sponsors.id', '=', 'at_pins.sponsor_id')
@@ -45,7 +45,7 @@ class FreebiesApiController extends BaseController
                             ->orderBy('at_pins.id', 'DESC')
                             ->get();
 
-        if (isset($freebies)) {
+        if ($freebies->first()) {
 
             foreach ($freebies as $key => $freebies) {
 
@@ -108,63 +108,57 @@ class FreebiesApiController extends BaseController
     {
 
         // Pages Details & SEO
-        $pages  = array(
-            'name'          => 'Sport News',
-            'title'         => 'All Sports News',
+         $pages  = array(
+            'name'          => 'Freebies - Airtime and Data Freebies',
+            'title'         => 'All Freebies - Airtime and Data Freebies',
             'description'   => ' ',
             'tags'          => '',
             'image'         => '',
             'author'        => config('app.name')
         );
         
-        $weather_node = $request->input('weather_node');
 
         if ($location_node) {
 
-            $weather            = Weather::all();
             $get_location       = Locations::where('node', $location_node)->first(); 
 
-            if ($get_location) {
+            if ($get_location->first()) {
 
-                if (!isset($weather_node)) {
+                $today          = Carbon::today();
+                $where_array    = array('at_pins.show_at' => $today->toDateString(), 'at_pins.is_active' => 1, 'at_pins.location_id' => $get_location->id);
+                $freebies       = DB::table('at_pins')
+                                    ->join('at_networks', 'at_networks.id', '=', 'at_pins.network_id')
+                                    ->join('sponsors', 'sponsors.id', '=', 'at_pins.sponsor_id')
+                                    ->where($where_array)
+                                    ->orderBy('at_pins.id', 'DESC')
+                                    ->get();
 
-                    $weather        = Weather::where('location_id', $get_location->id)->get(); 
-                    foreach ($weather as $weather) {
-                        $weather_array[]    = array(
-                            'name'          => $weather->name, 
-                            'node'          => $weather->node, 
-                            'description'   => $weather->description, 
-                            'bg_image'      => asset($weather->bg_image) 
+                if ($freebies->first()) {
+
+                    foreach ($freebies as $key => $freebies) {
+
+                        $freebies_array[] = array(
+                            'pin'                       => $freebies->pin_code, 
+                            'pin_reacharge_code'        => $freebies->code, 
+                            'pin_reacharge_code_char'   => $freebies->code_char, 
+                            'pin_code_char'             => $freebies->pin_code_char, 
+                            'pin_sponsor_image'         => asset($freebies->sponsor_promo_image), 
+                            'pin_sponsor_name'          => $freebies->name, 
+                            'pin_sponsor_logo'          => asset($freebies->logo_path)
                         );
+
                     }
-                    return $this->sendResponse($weather_array, 'Weather Data retrieved successfully.');
+                    
+                    return $this->sendResponse($freebies_array, 'Freebies (Airtime and Data) retrieved successfully.');
 
                 } else {
 
-                    $where_node     = array('location_id' => $get_location->id, 'node' => $weather_node);
-                    $weather_array  = Weather::where($where_node)->first(); 
-
-                    if (isset($weather_array)) {
-
-                        $weather_array   = array(
-                            'name'          => $weather_array->name, 
-                            'node'          => $weather_array->node, 
-                            'description'   => $weather_array->description, 
-                            'bg_image'      => asset($weather_array->bg_image) 
-                        );
-                        return $this->sendResponse($weather_array, 'Weather Node Data retrieved successfully.');
-
-                    } else {
-
-                        $errorMessages = array(
-                            'error_code'    => 500, 
-                            'error_message' => 'No Freebies available at the moment',
-                            'help_url' => config('app.url').'/api/freebies/'
-                        );
-                        return $this->sendError('Ooops, No Freebies available at the moment.', $errorMessages, 404);
-
-                    }
-
+                    $errorMessages = array(
+                        'error_code'    => 404, 
+                        'error_message' => 'No Freebies available at the moment on the current location.',
+                        'help_url' => config('app.url').'/api/freebies/'
+                    );
+                    return $this->sendError('Ooops, No Freebies available at the moment on the current location.', $errorMessages, 404);
 
                 }
 
