e30de05756002125e50522bb8c157de5b5da876f
haykins <akinsholasamuel@gmail.com>
2019-02-06 15:45:49 +0100

    Update Page titles

--- resources/views/layouts/dashboard/app.blade.php
+++ resources/views/layouts/dashboard/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ __('title') }} | {{ config('app.name', 'Laravel') }}</title>
+    <title>{{ $pages['title'] }} | {{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
     <!-- <link rel="dns-prefetch" href="//fonts.gstatic.com"> -->
