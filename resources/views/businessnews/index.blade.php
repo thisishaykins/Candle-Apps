46a965b86d44b8073b08009385d56ae1b11ee3ae
haykins <akinsholasamuel@gmail.com>
2019-02-28 10:25:49 +0100

    Updating Business News index view

--- resources/views/businessnews/index.blade.php
+++ resources/views/businessnews/index.blade.php
@@ -41,7 +41,8 @@
                                 @foreach($businessnews as $news)
                                   <tr>
                                     <th scope="row">--</th>
-                                    <?php $sponsor  = DB::table('sponsors')->where('id', '=', $news->sponsor_id)->first(); ?>
+                                    <?php $sponsor  = DB::table('sponsors')->where('id', '=', $news->sponsor_id)->first(); 
+                                    var_dump($sponsor); ?>
                                     <td>{{ $sponsor->name }}</td>
                                     <td>{{ $news->business_post_title }}</td>
                                     <td>{{ $news->business_post_content }}</td>
