df4b1c911ffb78cc70fdd4f09d2c8fc42d5ee109
haykins <akinsholasamuel@gmail.com>
2019-05-22 13:59:16 +0100

    update DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
-        $this->call(NetworksTableSeeder::class);
-        $this->call(StatusTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
+        // $this->call(NetworksTableSeeder::class);
+        // $this->call(StatusTableSeeder::class);
         $this->call(CandleCanalyticsTimeSeeder::class);
     }
 }
